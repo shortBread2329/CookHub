52ca8e7e6667e14702d3acb94d267832e0780f3d
shortBread2329 <shortBread1992@outlook.jp>
2021-06-29 08:14:13 +0000

    fix:Vue廃止

---
+++ app/app/models/ingredient.rb
@@ -0,0 +1,8 @@
+class Ingredient < ApplicationRecord
+  belongs_to :post_recipe, optional: true
+
+  with_options presence: true, on: :publicize do
+    validates :name
+    validates :amount
+  end
+end
