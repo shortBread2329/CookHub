18f02857a21f80a6f3d97a832fbdc03629f041ed
shortBread2329 <shortBread1992@outlook.jp>
2021-06-30 08:34:37 +0900

    Add:Controller

---
+++ app/app/controllers/admin/devise/sessions_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class Admin::Devise::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+
+  # ======= ゲストログイン機能 =========
+  def guest_sign_in
+    admin = Admin.guest
+    sign_in admin
+    redirect_to admin_root_path, notice: 'ゲスト・アカウント（管理者）でログインしました。'
+  end
+end
